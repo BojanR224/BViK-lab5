2110994b4c3686fce12b7894624d155c59f67149
BojanRobev <bojan.robev@gmail.com>
2022-05-10 17:25:54 +0200

    Initial commit

---
+++ contracts/Letter.sol
@@ -0,0 +1,21 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./node_modules/@openzeppelin/contracts/*";
+
+contract Letter {
+    uint256 private _value;
+
+    event ValueChanged(uint256 value);
+
+    function store(uint256 value) public {
+        _value = value;
+        emit ValueChanged(value);
+    }
+
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
